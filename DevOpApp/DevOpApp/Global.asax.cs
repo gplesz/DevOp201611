d6c41d6359c0057df0be126c0a6436fda2456085
gplesz <gabor.plesz@gmail.com>
2016-12-22 19:56:25 +0100

    #9: log4net kezdőkonfiguráció + naplózó sorok

--- DevOpApp/DevOpApp/Global.asax.cs
+++ DevOpApp/DevOpApp/Global.asax.cs
@@ -10,12 +10,46 @@ namespace DevOpApp
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            log4net.Config.XmlConfigurator.Configure();
+            using (log4net.ThreadContext.Stacks["NDC"].Push("Application_Start"))
+            {
+                log.DebugFormat("Begin: {0}",this.Context.Server);
+                AreaRegistration.RegisterAllAreas();
+                FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+                RouteConfig.RegisterRoutes(RouteTable.Routes);
+                BundleConfig.RegisterBundles(BundleTable.Bundles);
+            }
         }
+
+        protected void Application_End()
+        {
+            using (log4net.ThreadContext.Stacks["NDC"].Push("Application_End"))
+            {
+                log.DebugFormat("Begin: {0}", this.Context.Server);
+            }
+        }
+
+        protected void Application_BeginRequest()
+        {
+            using (log4net.ThreadContext.Stacks["NDC"].Push("Application_BeginRequest"))
+            {
+                log.DebugFormat("Begin: {0}", this.Request.RawUrl);
+            }
+        }
+
+        protected void Application_EndRequest()
+        {
+            using (log4net.ThreadContext.Stacks["NDC"].Push("Application_EndRequest"))
+            {
+                log.DebugFormat("Begin: {0}", this.Request.RawUrl);
+            }
+        }
+
+
+
     }
 }
